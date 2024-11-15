921f9f8765ac844b4b10f06ff21cfd595b3c2526
Brendan Haniff <107269432+funnierThan24@users.noreply.github.com>
2024-11-15 16:01:36 -0500

    Update login and create user API

--- server.js
+++ server.js
@@ -2,208 +2,116 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-app.use(express.json());
 
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://jgoreczky:a1070Ov216AesXmm@cluster0.h93x6.mongodb.net/COP4331?retryWrites=true&w=majority&appName=Cluster0';
+const url = "mongodb+srv://cop4331:62t9VFsPcmOqOAKb@cluster0.lamyt.mongodb.net/cop4331?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(url);
 client.connect();
 
-// Test the connection to the database
-client.connect()
-    .then(() => {
-        console.log('Successfully connected to MongoDB');
-        app.listen(5001, () => {
-            console.log("Server running at http://localhost:5000");
-        });
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
-    });
-
-
-var cardList =
-    [
-        'Roy Campanella',
-        'Paul Molitor',
-        'Tony Gwynn',
-        'Dennis Eckersley',
-        'Reggie Jackson',
-        'Gaylord Perry',
-        'Buck Leonard',
-        'Rollie Fingers',
-        'Charlie Gehringer',
-        'Wade Boggs',
-        'Carl Hubbell',
-        'Dave Winfield',
-        'Jackie Robinson',
-        'Ken Griffey, Jr.',
-        'Al Simmons',
-        'Chuck Klein',
-        'Mel Ott',
-        'Mark McGwire',
-        'Nolan Ryan',
-        'Ralph Kiner',
-        'Yogi Berra',
-        'Goose Goslin',
-        'Greg Maddux',
-        'Frankie Frisch',
-        'Ernie Banks',
-        'Ozzie Smith',
-        'Hank Greenberg',
-        'Kirby Puckett',
-        'Bob Feller',
-        'Dizzy Dean',
-        'Joe Jackson',
-        'Sam Crawford',
-        'Barry Bonds',
-        'Duke Snider',
-        'George Sisler',
-        'Ed Walsh',
-        'Tom Seaver',
-        'Willie Stargell',
-        'Bob Gibson',
-        'Brooks Robinson',
-        'Steve Carlton',
-        'Joe Medwick',
-        'Nap Lajoie',
-        'Cal Ripken, Jr.',
-        'Mike Schmidt',
-        'Eddie Murray',
-        'Tris Speaker',
-        'Al Kaline',
-        'Sandy Koufax',
-        'Willie Keeler',
-        'Pete Rose',
-        'Robin Roberts',
-        'Eddie Collins',
-        'Lefty Gomez',
-        'Lefty Grove',
-        'Carl Yastrzemski',
-        'Frank Robinson',
-        'Juan Marichal',
-        'Warren Spahn',
-        'Pie Traynor',
-        'Roberto Clemente',
-        'Harmon Killebrew',
-        'Satchel Paige',
-        'Eddie Plank',
-        'Josh Gibson',
-        'Oscar Charleston',
-        'Mickey Mantle',
-        'Cool Papa Bell',
-        'Johnny Bench',
-        'Mickey Cochrane',
-        'Jimmie Foxx',
-        'Jim Palmer',
-        'Cy Young',
-        'Eddie Mathews',
-        'Honus Wagner',
-        'Paul Waner',
-        'Grover Alexander',
-        'Rod Carew',
-        'Joe DiMaggio',
-        'Joe Morgan',
-        'Stan Musial',
-        'Bill Terry',
-        'Rogers Hornsby',
-        'Lou Brock',
-        'Ted Williams',
-        'Bill Dickey',
-        'Christy Mathewson',
-        'Willie McCovey',
-        'Lou Gehrig',
-        'George Brett',
-        'Hank Aaron',
-        'Harry Heilmann',
-        'Walter Johnson',
-        'Roger Clemens',
-        'Ty Cobb',
-        'Whitey Ford',
-        'Willie Mays',
-        'Rickey Henderson',
-        'Babe Ruth'
-    ];
-
 app.use(cors());
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-    );
-    next();
+app.use((req, res, next) =>
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
 });
 
-app.post('/api/addcard', async (req, res, next) => {
-    // incoming: userId, color
-    // outgoing: error
 
-    const { userId, card } = req.body;
+//Example API endpoints
 
-    const newCard = { Card: card, UserId: userId };
-    var error = '';
-
-    try {
-        const db = client.db();
-        const result = db.collection('Cards').insertOne(newCard);
-    }
-    catch (e) {
-        error = e.toString();
-    }
-
-    cardList.push(card);
-
-    var ret = { error: error };
-    res.status(200).json(ret);
+app.post('/api/createUser', async (req, res, next) =>
+{
+  // incoming: userId, color
+  // outgoing: error
+  const { login, password, email } = req.body;
+  const newUser= {Username:login, Password:password, Email:email, };
+  var error = '';
+  try
+  {
+    const db = client.db();
+    const result = db.collection('Users').insertOne(newUser);
+  }
+  catch(e)
+  {
+    error = e.toString();
+  }
+  cardList.push( card );
+  var ret = { error: error };
+  res.status(200).json(ret);
 });
 
-app.post('/api/login', async (req, res, next) => {
+
+app.post('/api/login', async (req, res, next) =>
+  {
     // incoming: login, password
     // outgoing: id, firstName, lastName, error
-
     var error = '';
-
     const { login, password } = req.body;
-
     const db = client.db();
-    const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
-
+    const results = await
+    db.collection('Users').find({Username:login,Password:password}).toArray();
     var id = -1;
     var fn = '';
     var ln = '';
-    var error = 'User/Password combination incorrect';
-
-    if (results.length > 0) {
-        id = results[0].UserId;
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
-        error = '';
+    if( results.length > 0 )
+    {
+      id = results[0].UserId;
+      fn = results[0].FirstName;
+      ln = results[0].LastName;
     }
-
-    var ret = { id: id, firstName: fn, lastName: ln, error };
+    var ret = { id:id, firstName:fn, lastName:ln, error:''};
     res.status(200).json(ret);
-});
+  });
 
-app.post('/api/searchcards', async (req, res, next) => {
-    // incoming: userId, search
-    // outgoing: results[], error
-    var error = '';
-    const { userId, search } = req.body;
-    var _search = search.trim();
-    const db = client.db();
-    const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*' } }).toArray();
-    var _ret = [];
-    for (var i = 0; i < results.length; i++) {
-        _ret.push(results[i].Card);
-    }
-    var ret = { results: _ret, error: error };
-    res.status(200).json(ret);
-});
+/*function LoggedInName()
+{
+  var _ud = localStorage.getItem('user_data');
+  if(_ud == null) _ud = "";
+  var ud = JSON.parse(_ud);
+  var userId = ud.id;
+  var firstName = ud.firstName;
+  var lastName = ud.lastName;
+  function doLogout(event:any) : void
+  {
+    event.preventDefault();
+    localStorage.removeItem("user_data")
+    window.location.href = '/';
+  };
+  return(
+    <div id="loggedInDiv">
+    <span id="userName">Logged In As {firstName} {lastName}</span><br />
+    <button type="button" id="logoutButton" className="buttons"
+    onClick={doLogout}> Log Out </button>
+    </div>
+  );
+};*/
+
+
+/*app.post('/api/searchcards', async (req, res, next) =>
+{
+  // incoming: userId, search
+  // outgoing: results[], error
+  var error = '';
+  const { userId, search } = req.body;
+  var _search = search.trim();
+  const db = client.db();
+  const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*'}}).toArray();
+  var _ret = [];
+  for( var i=0; i<results.length; i++ )
+  {
+    _ret.push( results[i].Card );
+  }
+  var ret = {results:_ret, error:error};
+  res.status(200).json(ret);
+});*/
 
 app.listen(5000); // start Node + Express server on port 5000
+
